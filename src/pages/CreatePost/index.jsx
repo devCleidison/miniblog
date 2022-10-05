1450cb0626b393d1afda0cf890335bc40e7ba222
Cleidison Silva <cleidison.dev@gmail.com>
2022-10-05 20:29:10 -0300

    feat: Function create new post added

--- src/pages/CreatePost/index.jsx
+++ src/pages/CreatePost/index.jsx
@@ -1,11 +1,15 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuthValue } from "../../context/AuthContext";
+import { useInsertDocument } from "../../hooks/useInsertDocument";
+
+import { Toast } from "../../components/Toast";
+import { toast } from "react-toastify";
 
 import { Form } from "../../components/Form";
+import { Input } from "../../components/Input";
 
 import { Container } from "./styles";
-import { Input } from "../../components/Input";
 
 export const CreatePost = () => {
   const [title, setTitle] = useState("");
@@ -14,16 +18,38 @@ export const CreatePost = () => {
   const [tags, setTags] = useState([]);
   const [formError, setFormError] = useState("");
 
-  const handleSubmit = (e) => {
+  const { user } = useAuthValue();
+
+  const { insertDocument, response } = useInsertDocument("posts");
+
+  const handleSavePost = (e) => {
     e.preventDefault();
+
+    setFormError("");
+
+    insertDocument({
+      title,
+      image,
+      body,
+      tags,
+      uid: user.uid,
+      createdBy: user.displayName,
+    });
+
+    if (response.error) {
+      setFormError(response.error);
+      toast.error(response.error, { className: "toast-alert" });
+    }
   };
 
   return (
     <Container>
+      <Toast />
+
       <h2>Create post</h2>
       <p>Write about whatever you want and share your knowledge!</p>
 
-      <Form onSubmit={handleSubmit}>
+      <Form handleSubmit={handleSavePost}>
         <Input
           placeholder="Title"
           value={title}
@@ -52,9 +78,8 @@ export const CreatePost = () => {
           required
         />
 
-        <Input type="submit" value="Save post" />
-        {/* {!loading && <Input type="submit" value="Save post" />}
-        {loading && <Input type="submit" value="Hold up" disabled />} */}
+        {!response.loading && <Input type="submit" value="Save post" />}
+        {response.loading && <Input type="submit" value="Hold up" disabled />}
       </Form>
     </Container>
   );
